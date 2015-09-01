dc4e2e6038738384d7be2b6fcdc393178d6df01a
mdiallo2 <mousto87@hotmail.fr>
2015-09-01 14:36:20 -0400

    .doc est changé

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<?php 
+<?php
+
 require_once 'data/_data.php';
 $emp_id = 102; // Employé "par défaut"
 // TODO: Lire et mettre à jour l'id de l'employé en Query string
+if(array_key_exists(PARAM_EMP_ID, $_GET)) {
+	$emp_id = $_GET[PARAM_EMP_ID];
+}
 $liste_employes = get_employes(); // Liste des noms de tous les employés
 $emp_data = $liste_employes[$emp_id];
 $agenda = get_agenda($emp_id);
+ //var_dump($agenda);
 ?>
 <html>
 	<head>
@@ -19,6 +24,14 @@ $agenda = get_agenda($emp_id);
 			<tr><th>Heure</th><th>Activité</th></tr>	
 <?php
 // TODO: Afficher l'agenda de l'employé
+
+
+            foreach ($agenda as $heure => $activité ) {
+				 echo '<tr><th>',$heure ,'</th><th>' , $activité , '</th></tr>';
+				//var_dump($heure);
+				//var_dump($activité);
+			}
+
 ?>
 		</table>
 	</div>
